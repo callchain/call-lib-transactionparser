f9bc6e81472e4e4fb9eb3a90810b0a91ade1b713
landoyjx <landoyjx@163.com>
2019-05-07 18:45:21 +0800

    first commit

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+lint() {
+  echo "eslint $(node_modules/.bin/eslint --version)"
+  #npm list babel-eslint | grep babel-eslint
+  curl "$REPO_URL/es6/eslintrc" | grep -v "no-var" > ./eslintrc
+  #echo "parser: babel-eslint" >> ./eslintrc
+  node_modules/.bin/eslint --no-eslintrc -c ./eslintrc $(git --no-pager diff --name-only -M100% --diff-filter=AM --relative $(git merge-base FETCH_HEAD origin/HEAD) FETCH_HEAD | grep "\.js$")
+}
+
+lint
+npm test
